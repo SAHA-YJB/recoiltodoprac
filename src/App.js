65de44fc4848d6ba2fba3fdc15e96e2bcc559311
SAHA-YJB <pkomom123@naver.com>
2023-07-20 19:14:30 +0900

    비동기 처리해보기

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { Suspense } from "react";
 import TodoItemCreator from "./components/TodoItemCreator";
 import { useRecoilValue } from "recoil";
-import { todoListAtom } from "./recoil/atom/todoListAtom";
+import {
+  filterdTodoListStateSelector,
+  todoListAtom,
+} from "./recoil/atom/todoListAtom";
 import TodoItem from "./components/TodoItem";
+import TodoFilters from "./components/TodoFilters";
+import TodoListStats from "./components/TodoListStats";
+import { currentUserNameQuery } from "./recoil/atom/userAtoms";
 
+const CurrentUserInfo = () => {
+  const userName = useRecoilValue(currentUserNameQuery);
+  return <div>userName: {userName}</div>;
+};
 const App = () => {
   //아톰의 값가져오기(값참조만)
   const allTodoList = useRecoilValue(todoListAtom);
+  const filterdTodoList = useRecoilValue(filterdTodoListStateSelector);
 
   return (
     <div>
+      <Suspense fallback={<div>로딩 중</div>}>
+        <CurrentUserInfo />
+      </Suspense>
+      <TodoListStats />
+      <TodoFilters />
       <TodoItemCreator />
-      {allTodoList.map((item) => {
+      {filterdTodoList.map((item) => {
         return <TodoItem key={item.id} item={item} />;
       })}
     </div>
